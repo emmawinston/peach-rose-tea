a961307e17117a130ab9255c730b77baa932b4d9
Emma Winston <emma.winston@gmail.com>
2016-05-02 15:48:48 +0100

    initial commit

---
+++ peachrosetea.py
@@ -0,0 +1,11 @@
+import tweepy
+import pycorpora
+import random
+
+
+
+# print a random flower name
+
+print random.choice(pycorpora.foods.fruits['fruits'])
+print random.choice(pycorpora.plants.flowers['flowers'])
+print random.choice(pycorpora.foods.tea['teas'])
