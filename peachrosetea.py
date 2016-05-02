09543590844fde09b91a659f00df581f9d5beeba
Emma Winston <emma.winston@gmail.com>
2016-05-02 15:56:03 +0100

    including initial twitter stuff

--- peachrosetea.py
+++ peachrosetea.py
@@ -9,3 +9,55 @@ import random
 print random.choice(pycorpora.foods.fruits['fruits'])
 print random.choice(pycorpora.plants.flowers['flowers'])
 print random.choice(pycorpora.foods.tea['teas'])
+
+
+OAUTH_SECRET    = "foo"
+
+OAUTH_TOKEN     = "foo"
+
+CONSUMER_KEY    = "foo"
+
+CONSUMER_SECRET = "foo"
+
+
+#################################### term searches ######################################################
+#### PEACH #####
+
+def track():
+
+  # Prompt for login credentials and setup stream object
+
+  TRACK_TERM = 'peach'
+
+  api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
+
+  f = api.request('statuses/filter', {'track': TRACK_TERM})
+
+  for item in f:
+    if 'text' in item:
+
+      # print item['text'].split()
+      splitTweet = item['text'].split()
+      
+      counts = []
+
+      for t in splitTweet:
+        try:
+          counts.append(dictionary[t.lower()])
+        except: 
+          print ''
+
+      item['counts'] = counts
+
+
+      if 'retweeted_status' in item:
+
+        item2 = item['retweeted_status']
+        #print(item2['favorite_count'])
+        #splitTweet = item2['favorite_count'].split()
+
+        favorites = []
+
+        favorites.append(item2['favorite_count'])
+      
+        sendOSC(item)
